06e1a24f28c731d4e339ae414fd53233a1878700
sachaMorin <morin.sacha@gmail.com>
2023-06-14 12:50:46 -0400

    Fix obtacle tracking problem

--- src/nodes/map_broadcaster.py
+++ src/nodes/map_broadcaster.py
@@ -7,11 +7,13 @@ The dict is encoded as a string with JSON. A proper ROS message would be better,
 require a UDP interface.
 
 The map is published both to a ROS topic and to a UDP port."""
+import tf.transformations as ts
 import roslib
 import rospy
 import tf
 import json
 
+import numpy as np
 import socket
 import sys
 import json
@@ -27,24 +29,44 @@ if __name__ == '__main__':
     rospy.init_node('map_broadcaster')
     listener = tf.TransformListener()
     map_pub = rospy.Publisher('map', String, queue_size=10)
+    dict_map_frame = dict()
 
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
         message = dict()
 
-        # Lookup required transforms
-        for obj in ['/board0']:
-            # TODO BEGIN MRSS: Look up a relevant transform
+        # Update required transforms
+        for obj in ['/rig', '/board0', '/board1', '/board2', '/board3', '/obstacle1', '/obstacle2', '/obstacle3']:
             try:
-                (trans,_) = listener.lookupTransform('/board0', obj, rospy.Time(0))
+                (trans,rot) = listener.lookupTransform('/map', obj, rospy.Time(0))
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
+            dict_map_frame[obj] = dict()
+            dict_map_frame[obj]["trans"] = trans
+            dict_map_frame[obj]["rot"] = rot
 
-            # End MRSS
+        # Build map in rig frame and save them to the map message
+        message = dict()
+        if '/rig' not in dict_map_frame:
+            continue
 
-            # TODO BEGIN MRSS: Add transform to Dict
-            message[obj] = [trans[0], trans[1]]
-            # End MRSS
+        trans_robot = dict_map_frame['/rig']['trans']
+        rot_robot = dict_map_frame['/rig']['rot']
+        rig_transform = ts.concatenate_matrices(ts.translation_matrix(trans_robot), ts.quaternion_matrix(rot_robot))
+        inv_rig = np.array(ts.inverse_matrix(rig_transform))
+
+        # Put all other transforms in robot frame
+        for key, value in dict_map_frame.items():
+            if key is not '/rig':
+                trans = value['trans']
+                rot = value['rot']
+                transform = ts.concatenate_matrices(ts.translation_matrix(trans), ts.quaternion_matrix(rot))
+                transform = np.array(transform)
+                transform_rig_frame = inv_rig @ transform
+                trans = ts.translation_from_matrix(transform_rig_frame)
+                rot = ts.quaternion_from_matrix(transform_rig_frame)
+
+            message[key] = [trans[0], trans[1]]
 
 
         # Add timestamp
