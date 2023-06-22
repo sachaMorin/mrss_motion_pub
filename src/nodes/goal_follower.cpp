7a585c2e8ceafe9496b9c49d72886db45b2c1e16
sachaMorin <morin.sacha@gmail.com>
2023-06-22 10:30:44 -0400

    cmds and launch file

--- src/nodes/goal_follower.cpp
+++ src/nodes/goal_follower.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
 
   ros::NodeHandle node;
 
-  ros::Publisher pub = node.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+  ros::Publisher pub = node.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
   tf::TransformListener listener;
 
@@ -22,12 +22,35 @@ int main(int argc, char** argv){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
-    geometry_msgs::Twist msg;
-    msg.linear.x = 123.;
-    msg.linear.y = 123.;
-    msg.angular.z = 123;
 
-    pub.publish(msg);
+    // Send commands
+    double angular = atan2(transform.getOrigin().y(), transform.getOrigin().x());
+    double linear = sqrt(pow(transform.getOrigin().x(), 2) + pow(transform.getOrigin().y(), 2));
+    geometry_msgs::Twist cmd;
+
+    if (linear > 0.2)
+    {
+        cmd.angular.z = std::max(std::min(angular, 0.5), -0.5);
+
+        // Only move forward if little to no angular velocity
+        if (std::abs(cmd.angular.z) < 0.35)
+            cmd.linear.x = std::min(0.15, linear);
+        else
+            cmd.linear.x = 0.0;
+
+        pub.publish(cmd);
+        ROS_WARN("GO");
+        ROS_WARN_STREAM("ANGULAR: " << angular);
+        ROS_WARN_STREAM("LINEAR: " << linear);
+    }
+    else
+    {
+        // Empty command
+        pub.publish(cmd);
+        ROS_ERROR("STOP");
+        ROS_ERROR_STREAM("ANGULAR: " << angular);
+        ROS_ERROR_STREAM("LINEAR: " << linear);
+    }
 
     rate.sleep();
   }
